bbb568e2531890f99ec3691b6350f78ea637ad2b
dr-prussia <92026278+dr-prussia@users.noreply.github.com>
2024-03-07 16:31:25 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -16,6 +16,31 @@ app.$mount()
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+//导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+//请求根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+//请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+//响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+//封装弹窗的方法
+uni.$showMsg = function(title = '数据请求失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+
 export function createApp() {
   const app = createSSRApp(App)
   return {
